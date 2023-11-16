9be54a031db6b926b3ffb89ea1253440abb99cc5
Oracle Public Cloud User <opc@stardust.sub07171735450.utdallas.oraclevcn.com>
2023-11-16 22:48:27 +0000

    Updated readme

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 This repository contains the code for the project - **Probabilistic Multi-Modal Discriminative Learning**
 
 ## Setup
+Create a new virtual environment and install the required packages given in `requirements.txt`.
+
 - Submodule Dependencies
+
+## To Run
+Specify the hyperparameter configurations for your experiment in the appropriate config file inside `conf/`. 
+Use the following command to run experiments. Ypou can pass values as needed from the command line for the hyperparameters specified in the config file.
+
+```bash
+python main.py dataset=avmnist experiment=avmnist_weighted_mean batch_size=128
+```
--- models/fusion.py
+++ models/fusion.py
@@ -29,25 +29,13 @@ class EinsumNet(torch.nn.Module):
     """Implements a module that returns performs weighted mean."""
     
     def __init__(self, num_features, classes, leaves=10, sums=10, num_splits=10, dropout=0.0):
-        super(WeightedMean, self).__init__()
-        self.weights = torch.nn.Parameter(torch.randn(size=tuple(weight_dims)))
-        self.normalize_dim = normalize_dim
-        self.softmax = torch.nn.Softmax(dim=normalize_dim)
-        
-    def forward(self, x):
-        x = (x*self.softmax(self.weights)).sum(self.normalize_dim )
-        return x
+        super(EinsumNet, self).__init__()
+        raise NotImplementedError
 
 
 class FlowCircuit(torch.nn.Module):
     """Implements a module that returns performs weighted mean."""
     
-    def __init__(self, weight_dims, normalize_dim):
-        super(WeightedMean, self).__init__()
-        self.weights = torch.nn.Parameter(torch.randn(size=tuple(weight_dims)))
-        self.normalize_dim = normalize_dim
-        self.softmax = torch.nn.Softmax(dim=normalize_dim)
-        
-    def forward(self, x):
-        x = (x*self.softmax(self.weights)).sum(self.normalize_dim )
-        return x
+    def __init__(self, num_features, classes, leaves=10, sums=10, num_splits=10, dropout=0.0):
+        super(FlowCircuit, self).__init__()
+        raise NotImplementedError
